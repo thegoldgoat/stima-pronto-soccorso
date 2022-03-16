03436ac15b52724d615f0ccb8c1a56c4dae48a72
Riccardo Briccola <riccardo.briccola00@gmail.com>
2022-03-16 12:32:09 +0100

    Comments fix

--- src/common/Queue/therapy_queue.py
+++ src/common/Queue/therapy_queue.py
@@ -7,11 +7,11 @@ class TherapyQueue():
         self.therapy_queue = MyPriorityQueue()
 
     def push(self, new_patient: TherapyPatient):
-        # Add patient to the therapy queue
+        """ Add patient to the therapy queue """
         self.therapy_queue.push(new_patient)
 
     def create_copy_and_generate(self):
-        # Create a deep copy, generate variabili aleatorie and return
+        """ Create a deep copy, generate variabili aleatorie and return """
         return_value = TherapyQueue()
         return_value.therapy_queue = MyPriorityQueue(
             heap=[therapyPatient.clone_and_generate() for therapyPatient in self.therapy_queue.heap])
--- src/common/therapyPatient.py
+++ src/common/therapyPatient.py
@@ -6,14 +6,14 @@ class TherapyPatient():
         self.id = id
         self.therapy_generator = therapy_generator
 
-        # If therapy time has been generated before I save it
+        # If therapy time has been generated before (for example in a Patient) I save it
         self.therapy_time = therapy_time 
         
-        # If I received only the therapy_generator I generate instantly the therapy_time
+        # If I received only the therapy_generator I generate instantly the therapy_time 
+        # The therapy time is generated instantly because the queue will order itself by TherapyPatient.therapy_time
         if self.therapy_time is None:
             self.generate_therapy()
         
-        # The therapy time is generated instantly because the queue will order itself by TherapyPatient.therapy_time
 
     def generate_therapy(self):
         """ Generate the therapy time using the therapy generator """
