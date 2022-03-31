cf8f3e9475982cfb7116c00ecb033289d5839841
Andrea Somaini <andrea.somaini@mail.polimi.it>
2022-03-31 20:55:18 +0200

    DB population example and api for patient

--- restapi/src/routes/patient/index.ts
+++ restapi/src/routes/patient/index.ts
@@ -1,4 +1,6 @@
 import { Router, Request, Response } from 'express'
+import { simulationModel } from '../../models/simulation'
+import { esteemModel } from '../../models/esteem'
 
 const patientRoutes = Router()
 
@@ -7,7 +9,22 @@ patientRoutes.get('/:patientId', async (req: Request, res: Response) => {
 
   if (!patientId) return res.sendStatus(401)
 
-  res.send('Test')
+  const latestSimulation = await simulationModel.find().limit(1).sort({
+    simulation_time: -1,
+  })
+
+  if (!latestSimulation || latestSimulation.length === 0)
+    return res.status(404).send('No simulation found')
+
+  const patientEsteem = await esteemModel.findOne({
+    patient_id: patientId,
+    simulation_id: latestSimulation[0]._id,
+  })
+
+  if (!patientEsteem)
+    return res.status(404).send('Cannot find any simulation for this patient')
+
+  return res.json(patientEsteem.waiting_times)
 })
 
 export default patientRoutes
