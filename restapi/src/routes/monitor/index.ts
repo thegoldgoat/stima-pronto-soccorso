139c487787f66ce08238297751807de1da4020a3
Andrea Somaini <andrea.somaini@mail.polimi.it>
2022-04-01 11:13:17 +0200

    Convert esteem for /monitor/all

--- restapi/src/routes/monitor/index.ts
+++ restapi/src/routes/monitor/index.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from 'express'
-import { esteemModel } from '../../models/esteem'
+import { Esteem, esteemModel } from '../../models/esteem'
 import { waitingPatientModel } from '../../models/waiting_patient'
 import { getLatestSimulationId } from '../../utils/simulation'
 
@@ -9,6 +9,32 @@ monitorRoutes.use(async (req: Request, res: Response, next) => {
   // TODO: Security (?) Only the monitors can access
   next()
 })
+
+type PatientEsteem = Esteem & { emergency_code: Number; arrival_time: Date }
+
+async function populatePatientId(esteem: PatientEsteem) {
+  console.debug('esteem before', esteem)
+
+  const waitingPatient = await waitingPatientModel.findOne(
+    { patient_id: esteem.patient_id },
+    { _id: 0, emergency_code: 1, arrival_time: 1 }
+  )
+
+  if (!waitingPatient)
+    throw new Error(
+      'Unable to find waiting patient for ID=' + esteem.patient_id
+    )
+
+  console.debug('waitingPatient', waitingPatient)
+
+  esteem.emergency_code = waitingPatient.emergency_code
+  esteem.arrival_time = waitingPatient.arrival_time
+
+  // TODO: ecmascript 2019 (?)
+  esteem.waiting_times = Object.fromEntries(esteem.waiting_times)
+
+  console.debug('esteem after', esteem)
+}
 
 monitorRoutes.get('/all', async (req: Request, res: Response) => {
   let latestSimulationId
@@ -25,7 +51,18 @@ monitorRoutes.get('/all', async (req: Request, res: Response) => {
 
   if (!latestEsteems) return res.status(404).send('Cannot get latests esteems')
 
-  return res.json(latestEsteems)
+  const esteemsObject = latestEsteems.map((esteem) => esteem.toObject())
+
+  const allPromises: Promise<void>[] = []
+  esteemsObject.forEach((esteem) => {
+    allPromises.push(populatePatientId(esteem as any))
+  })
+
+  await Promise.all(allPromises)
+
+  console.debug('esteemsObject', esteemsObject)
+
+  return res.json(esteemsObject)
 })
 
 export default monitorRoutes
