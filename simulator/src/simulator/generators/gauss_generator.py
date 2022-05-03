f1de6279deac210e37f5ab914edb2b55d75b04b2
Andrea Somaini <andrea.somaini@mail.polimi.it>
2022-05-03 17:35:22 +0200

    Generating exponential an Normal with standard lib

--- simulator/src/simulator/generators/gauss_generator.py
+++ simulator/src/simulator/generators/gauss_generator.py
@@ -1,6 +1,5 @@
 from src.simulator.generators.generator import Generator
-import numpy as np
-from math import floor
+from random import normalvariate
 
 
 class GaussGenerator(Generator):
@@ -10,6 +9,9 @@ class GaussGenerator(Generator):
         self.variance = variance
 
     def generate_sample(self):
-        # TODO: to implement
-        # value can't be less than zero
-        return floor(max(1, np.random.default_rng().normal(self.average, self.variance)))
+        sample = -1
+
+        while sample < 0:
+            sample = normalvariate(self.average, self.variance)
+
+        return round(sample)
